9b4f0204b68f05f9e31146c672df4cd623bbd448
yaminicharan25 <yaminicharan25@gmail.com>
2026-09-03 12:51:36 +0530

    Add files via upload

---
+++ arthematic operator.py
@@ -0,0 +1,12 @@
+# Arithmetic operators
+
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)     
+print("Multiplication:", a * b)  
+print("Division:", a / b)        
+print("Floor Division:", a // b)  
+print("Modulus:", a % b)          
+print("Exponentiation:", a ** b)  
